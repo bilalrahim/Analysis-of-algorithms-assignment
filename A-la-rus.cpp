da68d6adec43f7d428a36f7123d6b98ed2acfaa7
bilalrahim <bilalrahim@users.noreply.github.com>
2019-08-29 23:23:06 +0500

    Create A-la-rus.cpp

---
+++ A-la-rus.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int a_la_ruse(int m,int n)
+
+{	
+  	int temp=0;
+  	while(m!=1)
+  	{
+    
+    	if(m%2==1)
+    	{
+      		temp+=n;
+    	}
+	m=m/2;
+    n=n*2;
+  }
+    temp+=n;
+	return temp;
+}
+int main(){
+	clock_t tStart = clock();
+	out<<a_la_ruse(1234,981)<<endl;
+	printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+ 	return 0;
+}
+
